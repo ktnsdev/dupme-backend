2d3ee1d9092b55a50ff9efbb3a64af5ec52bd450
Napat Asavarojkul <puttersnap@icloud.com>
2021-10-11 09:59:57 +0700

    fix: status code

--- src/controllers/room/create-room.js
+++ src/controllers/room/create-room.js
@@ -2,16 +2,17 @@
 
 const dayjs = require("dayjs");
 const { logger } = require("../../configs/config");
+const APIStatus = require("../../configs/api-errors");
 
 function createRoom(req, res) {
     if (req.query.uuid === undefined || req.query.uuid === null || req.query.uuid === "") {
         logger.error("400 Bad request from the client");
         logger.error("UUID not found.");
-        return res.status(400).json({ status: 400, message: "Bad Request" });
+        return res.status(APIStatus.BAD_REQUEST.status).json(APIStatus.BAD_REQUEST);
     }
     if (req.query.uuid === "") {
         logger.error("User not found.");
-        return res.status(400).json({ status: 400, message: "Bad Request" });
+        return res.status(APIStatus.INTERNAL.USERNAME_USED.status).json(APIStatus.INTERNAL);
     }
     //check UUID with database if it has ok if not return error
     let roomID;
@@ -24,8 +25,8 @@ function createRoom(req, res) {
     let date = new dayjs();
     //put inside database json{room_id: xxxx, host: xxxx, time_created: xxxx}
 
-    return res.status(200).json({
-        status: 200,
+    return res.status(APIStatus.OK.status).json({
+        status: APIStatus.OK.status,
         message: "Room created successfully",
         room: { room_id: roomID, host: req.query.uuid, time_created: date.format() },
     });
