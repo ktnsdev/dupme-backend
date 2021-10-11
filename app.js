0978a00e007b32526ce1fc4ac8adde061a6c4be0
ktns <pom-2551@hotmail.com>
2021-10-11 22:38:03 +0700

    fix: app.js now use port from .env

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Route = require("./src/routes/routes.js");
 const Router = Route(app);
 Router.setup();
 
-app.listen(3000, () => {
-    console.log("Starting server on port :3000...");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Starting server on port " + PORT);
 });
